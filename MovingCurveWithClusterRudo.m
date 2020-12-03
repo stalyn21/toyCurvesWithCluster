1d3672c67e5f9df3723585a8b55f3552c96d9184
Stalyn Chancay <stalyn151996@gmail.com>
2020-12-03 01:09:52 -0500

    Functions of the seed for the numbers random

--- MovingCurveWithClusterRudo.m
+++ MovingCurveWithClusterRudo.m
@@ -1,33 +1,37 @@
 clear all, clc, close all
-%This is a change to try Branch "Branchtry" 
-N      = 150; %Number of *
-Nf= 10; %number of frames
-n      = 1:N/2;
-tao    = n/N;
-v=0.5;
-%X=rand(2*length(tao),Nf+1);%Matriz random
-Y=rand(2*length(tao),Nf);%Matriz random
 
+function rng(x)
+  randn("seed",x)
+  rand("seed",x)
+end
+
+%Un comentario en el cod para variar
+N = 150; %Number of *
+Nf = 10; %number of frames
+n = 1:N/2;
+tao = n/N;
+v=0.5;
+s = 13; %seed for the random numbers
+%X=rand(2*length(tao),Nf+1);%Matriz random
+rng(s);
+Y=rand(2*length(tao),Nf);%Matriz random
 x = abs(cos(2*pi*tao))'; 
-    x = [x;-x];
-    x= double(x);
-    size(x);
+x = [x;-x];
+x= double(x);
+size(x);
 %X(:,1)= x; %Reemplazar la primera columna de X por x.
-rng(1,'twister');
-s = rng;
 
 for t = 1:Nf 
-    
     yt = abs(t*sin(2*pi*tao))';
     yt = [yt;yt];
     rng(s);
     mu = rand(length(yt),1);
     yt = yt + v*mu;
-    Y(:,t) = yt; %La columna t+1 es reemplazado por yt. 
+    Y(:,t) = yt; %La columna t es reemplazado por yt. 
     plot(x,yt,'*r')
     axis([-1 1 0 Nf+Nf/10])
     hold on
-    %pause(0.2)
+    pause(0.2)
 end
 
 x;
