02dd94a39b289cc00241bbcd063ee20f9ff733dd
stalyn <stalyn151996@gmail.com>
2020-12-18 15:53:42 -0500

    updated source to work in Octave an Matlab together

--- newkmeans.m
+++ newkmeans.m
@@ -1,58 +1,63 @@
-function  [assignments, centers] = newkmeans(X, k, centers = 0, maxiter = 200)
-	if (centers == 0)
-		centerRows = randperm(size(X)(1));
-		centers = X(centerRows(1:k), :);
-	endif
-	numOfRows = length(X(:,1));
-	numOfFeatures = length(X(1,:));
-	assignments = ones(1, numOfRows);
+function  [assignments, centers] = newkmeans(X, k)
+  centers = 0; 
+  maxiter = 200;
+  s = 4;
+  if (centers == 0)
+    %seed(s);
+    centerRows = randperm(size(X,1));
+    centers = X(centerRows(1:k), :);
+  end
+  numOfRows = length(X(:,1));
+  numOfFeatures = length(X(1,:));
+  assignments = ones(1, numOfRows);
 
-	for iter = 1:maxiter
-		clusterTotals = zeros(k, numOfFeatures);
-		clusterSizes = zeros(k, 1);
-		for rowIx = 1:numOfRows
-			minDist = realmax;
-			assignTo = 0;
-			for centerIx = 1:k 
-				% Euclidian distance is used.
-				dist = sqrt(sum((X(rowIx, : ) - centers(centerIx, :)).^2));
-				if dist < minDist
-					minDist = dist;
-					assignTo = centerIx;
-				endif
-			endfor
-			assignments(rowIx) = assignTo;
+  for iter = 1:maxiter
+    clusterTotals = zeros(k, numOfFeatures);
+    clusterSizes = zeros(k, 1);
+    for rowIx = 1:numOfRows
+     minDist = realmax;
+     assignTo = 0;
+     for centerIx = 1:k 
+      % Euclidian distance is used.
+      dist = sqrt(sum((X(rowIx, : ) - centers(centerIx, :)).^2));
+      if dist < minDist
+       minDist = dist;
+       assignTo = centerIx;
+      end
+     end
+     assignments(rowIx) = assignTo;
 
-			% Keep these information to calculate cluster centers.
-			clusterTotals(assignTo, :) += X(rowIx, :);
-			clusterSizes(assignTo)++;
-		endfor
+     % Keep these information to calculate cluster centers.
+     clusterTotals(assignTo, :) = clusterTotals(assignTo, :) + X(rowIx, :);
+     clusterSizes(assignTo) = 1 + clusterSizes(assignTo);
+    end
 
-		% This process is called 'singleton' in terms of Matlab. 
-		% If a cluster is empty choose a random data point as new 
-		% cluster cener.
-		for clusterIx = 1:k
-			if (clusterSizes(clusterIx) == 0)
-				randomRow = round(1 + rand() * (numOfRows - 1) );
-				clusterTotals(clusterIx, :) =  X(randomRow, :);
-				clusterSizes(clusterIx) = 1;
-			endif
-		endfor
+    % This process is called 'singleton' in terms of Matlab. 
+    % If a cluster is empty choose a random data point as new 
+    % cluster cener.
+    for clusterIx = 1:k
+     if (clusterSizes(clusterIx) == 0)
+       %seed(s);
+       randomRow = round(1 + rand() * (numOfRows - 1) );
+       clusterTotals(clusterIx, :) =  X(randomRow, :);
+       clusterSizes(clusterIx) = 1;
+      end
+    end
 
-		newCenters = zeros(k, numOfFeatures);
-		for centerIx = 1:k 
-			newCenters(centerIx, :) = clusterTotals(centerIx, : ) / clusterSizes(centerIx);
-		endfor
-	
-		diff = sum(sum(abs(newCenters - centers)));
-	
-		if diff < eps
-			%disp('Centers are same, which means we converged before maxiteration count. This is a good thing!')
-			break;
-		endif
-	
-		centers = newCenters;
-	endfor	
-	assignments = assignments';
-	%printf('iter: %d, diff: %f\n', iter, diff);
- endfunction
+    newCenters = zeros(k, numOfFeatures);
+    for centerIx = 1:k 
+     newCenters(centerIx, :) = clusterTotals(centerIx, : ) / clusterSizes(centerIx);
+    end
+   
+    diff = sum(sum(abs(newCenters - centers)));
+   
+    if diff < eps
+     %disp('Centers are same, which means we converged before maxiteration count. This is a good thing!')
+     break;
+    end
+   
+    centers = newCenters;
+  end 
+  assignments = assignments';
+   %printf('iter: %d, diff: %f\n', iter, diff);
+end
--- toy_data_generation_1.m
+++ toy_data_generation_1.m
@@ -1,10 +1,10 @@
 function [X] = toy_data_generation_1(Nf,N,K)
-    
-    colors = ["*r";"*g";"*b";"*y";"*m";"*c";"*w";"*k"];
+  
+    colors = [".r";".g";".b";".y";".m";".c";".k";".w"];
     n      = 1:N/2; 
     tao    = n/N; 
-    v      = 0;
-    s      = 7;
+    v      = 0.0;
+    s      = 13;
     X = [];
     xt     = ... 
         [abs(cos(2*pi*tao))';... 
@@ -15,27 +15,41 @@ function [X] = toy_data_generation_1(Nf,N,K)
         yt     = ... 
         [abs(t*sin(2*pi*tao))';... 
         abs(t*sin(2*pi*tao))']; 
-        seed(s);
-        yt += v*rand(length(yt),1);
+        %seed(s);
+        yt = yt + v*rand(length(yt),1);
 
         X = [X; xt yt];
 
         %plot(xt,yt,'*r'), hold on, pause(0.1) 
         %axis([min(xt) max(xt) 1 10]) 
-    endfor
+    end
+    
+    %Normalize matrix X
+    X = X./repmat(max(abs(X)),size(X,1),1);
     
     % Perform clustering
     [idx, centers] = newkmeans (X, K);
     
+    %Compute Fisher Criterion J
+    J_n = 0; 
+    J_d = 0;
+    for i = 1:K
+       J_n = J_n + (trace( (X(idx==i)-mean(X))*(X(idx==i)-mean(X))'));
+       J_d = J_d + trace(cov(X(idx==i)));
+    end
+    J = J_n / J_d;
+    
     %plotting data with clustering
     figure;
     for i = 1:K
+      axis([-1, 1, -0.1, 1.1]);
       plot (X (idx==i, 1), X (idx==i, 2), colors(i,:));
       hold on;
-      plot (centers (:, 1), centers (:, 2), 'kv', 'markersize', 10);
+      %plot (centers (:, 1), centers (:, 2), 'kv', 'markersize', 10);
       pause(0.1)
-    endfor
+    end
     hold off;
-   % fprintf([repmat('%f\t', 1, size(X, 2)) '\n'], X');
-    print -djpg clustering31.png
-endfunction
+   %fprintf([repmat('%f\t', 1, size(X, 2)) '\n'], X');
+   %print -djpg clustering.jpg
+   %print -djpg experiment1_8_0.jpg
+end
